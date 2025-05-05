e6702ef3b9ad15dcb1985c508654648f15a07dc2
STEVEN A BOWMAN <stevenabowman@Mac.hsd1.mi.comcast.net>
2025-05-05 19:34:18 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './sections/Header.jsx'
+
+const App = () => {
+    return (
+        <main className="overflow-hidden">
+            <Header />
+        </main>
+    )
+}
+export default App
+
