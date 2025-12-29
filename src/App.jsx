9208672917dd70dc1ada8f735cbbad1f2b1541a0
steveghost33 <stevenabowman@STEVENs-MacBook-Pro.local>
2025-12-29 16:17:50 -0500

    Fix routes and use router links in header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+
 import Home from "./pages/Home.jsx";
 import WebProjects from "./pages/WebProjects.jsx";
 import TechSolutions from "./pages/TechSolutions.jsx";
@@ -8,20 +10,15 @@ import Legal from "./pages/Legal.jsx";
 function App() {
   return (
     <Routes>
-      {/* Home */}
       <Route path="/" element={<Home />} />
 
-      {/* Fix Render serving or landing on /index.html */}
+      {/* If Render or a link lands on /index.html, send it home */}
       <Route path="/index.html" element={<Navigate to="/" replace />} />
 
-      {/* Pages */}
       <Route path="/web-projects" element={<WebProjects />} />
       <Route path="/tech-solutions" element={<TechSolutions />} />
       <Route path="/terms-of-use" element={<TermsOfUse />} />
       <Route path="/legal" element={<Legal />} />
-
-      {/* Fallback: never blank on unknown routes */}
-      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
