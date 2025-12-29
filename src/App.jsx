c166768c616ccaac39fa3f0017151e0a4c0340bc
steveghost33 <stevenabowman@STEVENs-MacBook-Pro.local>
2025-12-29 15:47:15 -0500

    Pages fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import WebProjects from "./pages/WebProjects.jsx";
 import TechSolutions from "./pages/TechSolutions.jsx";
@@ -6,14 +6,24 @@ import TermsOfUse from "./pages/TermsOfUse.jsx";
 import Legal from "./pages/Legal.jsx";
 
 function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/web-projects" element={<WebProjects />} />
-            <Route path="/tech-solutions" element={<TechSolutions />} />
-            <Route path="/terms-of-use" element={<TermsOfUse />} />
-            <Route path="/legal" element={<Legal />} />
-            </Routes>
-        );
+  return (
+    <Routes>
+      {/* Home */}
+      <Route path="/" element={<Home />} />
+
+      {/* Fix Render serving or landing on /index.html */}
+      <Route path="/index.html" element={<Navigate to="/" replace />} />
+
+      {/* Pages */}
+      <Route path="/web-projects" element={<WebProjects />} />
+      <Route path="/tech-solutions" element={<TechSolutions />} />
+      <Route path="/terms-of-use" element={<TermsOfUse />} />
+      <Route path="/legal" element={<Legal />} />
+
+      {/* Fallback: never blank on unknown routes */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
 }
+
 export default App;
