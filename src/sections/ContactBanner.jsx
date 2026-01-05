af4a3c6fe757ec6c7d0584fe929a9f901d16a730
steveghost33 <stevenabowman@STEVENs-MacBook-Pro.local>
2026-01-05 15:42:47 -0500

    Contact Banner adjust

--- src/sections/ContactBanner.jsx
+++ src/sections/ContactBanner.jsx
@@ -3,38 +3,58 @@ import "./ContactBanner.css";
 
 function ContactBanner() {
   return (
-    <section className="contact-banner">
+    <section className="contact-banner" aria-label="Consultation banner">
       <div className="contact-scroll">
         <p>
-          Schedule Your Consultation Today! Call:{" "}
-          <a href="tel:+13134741772" className="contact-link">
-            (313) 474-1772
-          </a>{" "}
-          or E-Mail:{" "}
-          <a href="mailto:info@ellatechsolutions.com" className="contact-link">
-            info@ellatechsolutions.com
-          </a>{" "}
-          for more information.
+          <a href="/#contact" className="banner-cta">
+            Schedule your free 30 minute consultation
+          </a>
+          <span className="banner-sub">
+            Prefer to call or email?{" "}
+            <a href="tel:+13134741772" className="contact-link">
+              (313) 474-1772
+            </a>{" "}
+            or{" "}
+            <a href="mailto:info@ellatechsolutions.com" className="contact-link">
+              info@ellatechsolutions.com
+            </a>
+            .
+          </span>
+
           <span className="spacer">    </span>
-          Schedule Your Consultation Today! Call:{" "}
-          <a href="tel:+13134741772" className="contact-link">
-            (313) 474-1772
-          </a>{" "}
-          or E-Mail:{" "}
-          <a href="mailto:info@ellatechsolutions.com" className="contact-link">
-            info@ellatechsolutions.com
-          </a>{" "}
-          for more information.
+
+          <a href="/#contact" className="banner-cta">
+            Schedule your free 30 minute consultation
+          </a>
+          <span className="banner-sub">
+            Prefer to call or email?{" "}
+            <a href="tel:+13134741772" className="contact-link">
+              (313) 474-1772
+            </a>{" "}
+            or{" "}
+            <a href="mailto:info@ellatechsolutions.com" className="contact-link">
+              info@ellatechsolutions.com
+            </a>
+            .
+          </span>
+
           <span className="spacer">    </span>
-          Schedule Your Consultation Today! Call:{" "}
-          <a href="tel:+13134741772" className="contact-link">
-            (313) 474-1772
-          </a>{" "}
-          or E-Mail:{" "}
-          <a href="mailto:info@ellatechsolutions.com" className="contact-link">
-            info@ellatechsolutions.com
-          </a>{" "}
-          for more information.
+
+          <a href="/#contact" className="banner-cta">
+            Schedule your free 30 minute consultation
+          </a>
+          <span className="banner-sub">
+            Prefer to call or email?{" "}
+            <a href="tel:+13134741772" className="contact-link">
+              (313) 474-1772
+            </a>{" "}
+            or{" "}
+            <a href="mailto:info@ellatechsolutions.com" className="contact-link">
+              info@ellatechsolutions.com
+            </a>
+            .
+          </span>
+
           <span className="spacer">    </span>
         </p>
       </div>
